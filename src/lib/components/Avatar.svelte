8ef304db47879e6bcf008233fda9e56a90e9dcbf
elringo70 <elringo70@hotmail.com>
2024-03-04 16:31:28 -0600

    1.0

--- src/lib/components/Avatar.svelte
+++ src/lib/components/Avatar.svelte
@@ -1,3 +1,9 @@
 <div class="hidden xl:flex max-w-none">
-	<img src="./avatar.png" alt="" width="737px" height="678px" class="translate-z-0 w-full h-full" />
+	<img
+		src="./my-avatar.png"
+		alt=""
+		width="737px"
+		height="678px"
+		class="translate-z-0 w-full h-full"
+	/>
 </div>
