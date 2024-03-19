c52686348a38db4468f2cd6dd7e02636ffc28542
elringo70 <elringo70@hotmail.com>
2024-03-18 23:06:46 -0600

    Plans section

---
+++ src/lib/components/PlansSlider.svelte
@@ -0,0 +1,53 @@
+<script>
+	import { Splide, SplideSlide } from '@splidejs/svelte-splide';
+	import '@splidejs/svelte-splide/css';
+	import Icon from '@iconify/svelte';
+
+	const plansData = [
+		{
+			plan: 'Landing Page',
+			price: '$ 2,500',
+			description: ['1', '2', '3']
+		},
+		{
+			plan: 'Single Page Application',
+			price: '$ 3,500',
+			description: ['1', '2', '3']
+		},
+		{
+			plan: 'eCommerce',
+			price: '$ 8,000',
+			description: ['1', '2', '3']
+		},
+		{
+			plan: 'A medida',
+			price: 'Solicita cotización',
+			description: ['1', '2', '3']
+		}
+	];
+
+	const options = {
+		gap: 10,
+		pagination: false
+	};
+</script>
+
+<Splide {options} aria-label="Planes" class="min-h-[400px]">
+	{#each plansData as plan}
+		<SplideSlide>
+			<div
+				class="min-w-52 w-72 min-h-[400px] bg-transparent backdrop-blur-md border border-gray-600 rounded-md p-5 shadow-md hover:shadow-xl transition-all z-10 hover:scale-105"
+			>
+				<div class="text-accent text-lg text-center">{plan.plan}</div>
+				<div>{plan.price}</div>
+				<div>
+					<ul>
+						{#each plan.description as feature}
+							<li><p class="text-xs">{feature}</p></li>
+						{/each}
+					</ul>
+				</div>
+			</div>
+		</SplideSlide>
+	{/each}
+</Splide>
---
+++ src/routes/plans/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import Motion from 'svelte-motion/src/motion/MotionSSR.svelte';
+	import Icon from '@iconify/svelte';
+
+	import MetaTag from '$lib/components/MetaTag.svelte';
+	import WaButton from '$lib/components/WAButton.svelte';
+
+	import { fadeIn } from '$lib/utils/variants.js';
+	import Bulb from '$lib/components/Bulb.svelte';
+	import PlansSlider from '$lib/components/PlansSlider.svelte';
+</script>
+
+<svelte:head>
+	<MetaTag
+		title="Agencia de desarrollo de software | Contactanos"
+		description="Contacta a nuestra empresa, cuentanos tu idea y la desarrollaremos a las necesidades del mismo."
+	/>
+</svelte:head>
+
+<div class="bg-primary/60 h-full">
+	<div class="w-full h-full absolute right-0 bottom-0">
+		<div
+			class="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"
+		></div>
+		<Bulb />
+		<PlansSlider />
+	</div>
+</div>
