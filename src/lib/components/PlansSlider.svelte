62077f06714e2b21c4a4fda6469ed3d55ca69835
elringo70 <elringo70@hotmail.com>
2024-03-22 00:58:43 -0600

    Continue plans section

--- src/lib/components/PlansSlider.svelte
+++ src/lib/components/PlansSlider.svelte
@@ -6,23 +6,61 @@
 	const plansData = [
 		{
 			plan: 'Landing Page',
-			price: '$ 2,500',
-			description: ['1', '2', '3']
+			price: '2,500',
+			description: [
+				'Dominio incluido',
+				'SSL incluido',
+				'Hosting por 1 año',
+				'Cuentas de correo incluidas',
+				'Optimización de imágenes',
+				'Capacitación de uso',
+				'Formulario de contacto',
+				'Plugin de contacto mediante Whatsapp',
+				'Ancho de banda ilimitado',
+				'100GB de SSD',
+				'Rendimiento standard',
+				'Modo responsivo incluido',
+				'Optimización SEO'
+			]
 		},
 		{
 			plan: 'Single Page Application',
-			price: '$ 3,500',
-			description: ['1', '2', '3']
+			price: '3,500',
+			description: [
+				'Incluye características de Landing Page',
+				'5 secciones',
+				'Mayor rendimiento',
+				'Copias de seguridad diarias',
+				'Protección DDOS',
+				'Formulario de contacto',
+				'Chat de Whatsapp'
+			]
 		},
 		{
 			plan: 'eCommerce',
-			price: '$ 8,000',
-			description: ['1', '2', '3']
+			price: '8,000',
+			description: [
+				'Incluye características de Landing Page',
+				'Incluye características de SPA',
+				'Alta de productos',
+				'Adaptación de pasarela de pagos',
+				'Asesoría de métodos de envío',
+				'Opción para convertir en catalogo',
+				'Página autoadministrable',
+				'Hasta 6 secciones'
+			]
 		},
 		{
 			plan: 'A medida',
 			price: 'Solicita cotización',
-			description: ['1', '2', '3']
+			description: [
+				'API, REST y RESTFul',
+				'Consumo de API´s',
+				'Bases de Datos',
+				'Panel Dashboard',
+				'App´s Multiplataforma',
+				'Despliegue de aplicación'
+			]
 		}
 	];
 
@@ -49,12 +87,20 @@
 			<div
 				class="min-w-56 w-72 min-h-[400px] bg-transparent backdrop-blur-md border border-gray-600 rounded-md p-5 shadow-md hover:shadow-xl transition-all mx-auto"
 			>
-				<div class="text-accent text-xl text-center pb-5">{plan.plan}</div>
-				<div>{plan.price}</div>
+				<div class="text-accent text-xl text-center pb-4 font-medium">{plan.plan}</div>
+				<div class="flex items-center gap-x-2 pb-1">
+					<span class="text-accent">
+						<Icon icon="heroicons-outline:currency-dollar" />
+					</span>
+					{plan.price}
+				</div>
 				<div>
 					<ul>
 						{#each plan.description as feature}
-							<li><p class="text-xs">{feature}</p></li>
+							<li class="flex items-center even:py-1">
+								<Icon icon="bi:check" />
+								<p class="text-xs">{feature}</p>
+							</li>
 						{/each}
 					</ul>
 				</div>
